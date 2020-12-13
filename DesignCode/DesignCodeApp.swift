2467cf994a1db91858b9f10a48c05e316b31297a
Dmitriuze <dimabylikin@gmail.com>
2020-12-13 14:48:15 +0300

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Dmitry Bulykin on 13.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
