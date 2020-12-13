4a224f8371351b4e5f7b2bdbcb9b51ab74ba602e
Dmitriuze <dimabylikin@gmail.com>
2020-12-13 18:23:47 +0300

    Lesson 5 - Timing Curve and Delay Animation

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -11,47 +11,64 @@ struct ContentView: View {
     
     @State private var mainCardIsShown = false
     @State private var mainCardPosition = CGSize.zero
+    @State private var bottomCardIsShown = false
     
     var body: some View {
         ZStack {
             TitleView()
+                .opacity(self.bottomCardIsShown ? 0.6 : 1)
+                .offset(y: self.bottomCardIsShown ? -200 : 0)
                 .blur(radius: self.mainCardIsShown ? 20 : 0)
-                .animation(.default)
+                .animation(
+                    Animation
+                        .default
+                        .delay(0.1)
+                )
             
             BackCardView()
                 .background(self.mainCardIsShown ? Color.card3 : Color.card4)
-                .cornerRadius(20)
+                .clipShape(RoundedRectangle(cornerRadius: self.bottomCardIsShown ? 30 : 20, style: .continuous))
                 .shadow(radius: 20)
                 .offset(x: 0, y: self.mainCardIsShown ? -400 : -40)
                 .offset(x: self.mainCardPosition.width, y: self.mainCardPosition.height)
-                .scaleEffect(0.9)
+                .offset(y: self.bottomCardIsShown ? -165 : 0)
+                .scaleEffect(self.bottomCardIsShown ? 1 : 0.9)
                 .rotationEffect(.degrees(self.mainCardIsShown ? 0 : 10))
+                .rotationEffect(.degrees(self.bottomCardIsShown ? -10 : 0))
                 .rotation3DEffect(
-                    .degrees(10),
+                    .degrees(self.bottomCardIsShown ? 0 : 10),
                     axis: (x: 1.0, y: 0.0, z: 0.0))
                 .blendMode(.hardLight)
-                .animation(.easeInOut(duration: 0.4))
+                .animation(.easeInOut(duration: 0.5))
             
             BackCardView()
                 .background(self.mainCardIsShown ? Color.card4 : Color.card3)
-                .cornerRadius(20)
+                .clipShape(RoundedRectangle(cornerRadius: self.bottomCardIsShown ? 30 : 20, style: .continuous))
                 .shadow(radius: 20)
                 .offset(x: 0, y: self.mainCardIsShown ? -200 : -20)
                 .offset(x: self.mainCardPosition.width, y: self.mainCardPosition.height)
-                .scaleEffect(0.95)
+                .offset(y: self.bottomCardIsShown ? -120 : 0)
+                .scaleEffect(self.bottomCardIsShown ? 1.1 : 0.95)
                 .rotationEffect(.degrees(self.mainCardIsShown ? 0 : 5))
+                .rotationEffect(.degrees(self.bottomCardIsShown ? -5 : 0))
                 .rotation3DEffect(
-                    .degrees(5),
+                    .degrees(self.bottomCardIsShown ? 0 : 5),
                     axis: (x: 1.0, y: 0.0, z: 0.0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.3))
             
             CardView()
+                
+                .clipShape(RoundedRectangle(cornerRadius: self.bottomCardIsShown ? 30 : 20, style: .continuous))
+                .scaleEffect(self.bottomCardIsShown ? 1.2 : 1)
+                .shadow(radius: 20)
                 .offset(x: self.mainCardPosition.width, y: self.mainCardPosition.height)
+                .offset(y: self.bottomCardIsShown ? -100 : 0)
                 .animation(.spring(response: 0.4, dampingFraction: 0.6, blendDuration: 0))
                 .blendMode(.hardLight)
                 .onTapGesture {
-                    self.mainCardIsShown.toggle()
+                    //                    self.mainCardIsShown.toggle()
+                    self.bottomCardIsShown.toggle()
                 }
                 .gesture(
                     DragGesture()
@@ -66,8 +83,9 @@ struct ContentView: View {
                 )
             
             ButtonCardView()
+                .offset(x: 0, y: self.bottomCardIsShown ? 360 : 1000)
                 .blur(radius: self.mainCardIsShown ? 30 : 0)
-                .animation(.default)
+                .animation(.timingCurve(0.2,0.2,0.8,1, duration: 0.4))
         }
     }
 }
@@ -103,8 +121,6 @@ struct CardView: View {
         }
         .frame(width: 340, height: 220)
         .background(Color.black)
-        .cornerRadius(20)
-        .shadow(radius: 20)
     }
 }
 
@@ -153,6 +169,5 @@ struct ButtonCardView: View {
         .background(Color.white)
         .cornerRadius(30)
         .shadow(radius: 20)
-        .offset(x: 0, y: 650)
     }
 }
