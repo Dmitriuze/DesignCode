5b79d4624da2a6cf3c75b69ca0cf65b3c32fbdf3
Dmitriuze <dimabylikin@gmail.com>
2020-12-13 20:49:07 +0300

    Lesson 10 - Drag Progress and Tap Background

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var showProfile = false
+    @State private var viewState = CGSize.zero
+        
     var body: some View {
         ZStack {
             
@@ -41,16 +43,33 @@ struct HomeView: View {
             .background(Color.white)
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-            .offset(y: self.showProfile ? -450 : 0)
-            .rotation3DEffect(.degrees(self.showProfile ? -10 : 0),
+            .offset(y: self.showProfile ? self.viewState.height - 450 : 0)
+            .rotation3DEffect(.degrees(self.showProfile ? Double(self.viewState.height / 10) - 10 : 0),
                 axis: (x: 10.0, y: 0.0, z: 0.0))
             .scaleEffect(self.showProfile ? 0.9 : 1)
             .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             .edgesIgnoringSafeArea(.all)
             
             MenuView()
-                .offset(y: self.showProfile ? 0 : 600)
+                .background(Color.black.opacity(0.001))
+                .offset(y: self.showProfile ? 0 : 1000)
+                .offset(y: self.viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged {value in
+                            self.viewState = value.translation
+                        }
+                        .onEnded {value in
+                            if self.viewState.height > 50 {
+                                self.showProfile = false
+                            }
+                            self.viewState = .zero
+                        }
+                )
         }
     }
 }
