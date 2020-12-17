9164490a0a5bb9f3baf4c59deac40525d6b3636e
Dmitriuze <dimabylikin@gmail.com>
2020-12-17 13:43:17 +0300

    Part 2 Lesson 12 - Dynamic New View

--- DesignCode/Views/CourseListView.swift
+++ DesignCode/Views/CourseListView.swift
@@ -73,41 +73,18 @@ struct CourseView: View {
     
     var body: some View {
         ZStack(alignment: .top) {
-            
-            VStack(alignment: .leading,  spacing: 30) {
-                Text("Take your SwiftUI app to the App Store with advanced techniques like API data, packages and CMS.")
-                
-                Text("About this course").font(.title).bold()
-                
-                Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS. While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
-            }.padding(30)
-            .frame(maxWidth: course.show ? .infinity : screen.width - 60, maxHeight: course.show ? .infinity : 280, alignment: .top)
-            .offset(y: course.show ? screen.height / 2 + 10 : 0)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0.0, y: 20)
-            .opacity(course.show ? 1 : 0)
-            
             VStack {
                 HStack(alignment: .top) {
                     VStack(alignment: .leading, spacing: 20){
                         Text(course.title)
                             .font(.system(size: 24, weight: .bold))
+                            .lineLimit(2)
                         Text(course.subtitle)
                             .opacity(0.7)
                     }
                     Spacer()
-                    ZStack {
-                        Image(uiImage: course.logo).opacity(course.show ? 0 : 1)
-                        
-                        Image(systemName: "xmark")
-                            .font(.system(size: 16, weight: .medium))
-                            .foregroundColor(.white).frame(width: 40, height: 40, alignment: .center)
-                            .background(Color.black)
-                            .clipShape(Circle())
-                            .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0.0, y: 10)
-                            .opacity(course.show ? 1 : 0)
-                    }
+                    Image(uiImage: course.logo).opacity(course.show ? 0 : 1)
+                    
                 }.foregroundColor(Color.white)
                 Spacer()
                 
@@ -120,7 +97,8 @@ struct CourseView: View {
             }
             .padding(course.show ? 40 : 20)
             .padding(.top, course.show ? 30 : 0)
-            .frame(maxWidth: course.show ? .infinity : screen.width - 60, maxHeight: course.show ? screen.height / 2 : 280)
+            .frame(maxWidth: course.show ? .infinity : screen.width - 60)
+            .frame(height: course.show ? screen.height / 2 : 280)
             .background(Color(course.color))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color(course.color).opacity(0.3), radius: 20, x: 0.0, y: 20)
@@ -134,6 +112,12 @@ struct CourseView: View {
                 }
             }
             
+            if course.show {
+                CourseDetailView(course: $course, active: $active, activeIndex: $activeIndex, activeView: $activeView)
+                    .background(Color.white)
+                    .cornerRadius(30)
+            }
+            
             
         }
         .frame(height: course.show ? screen.height : 280)
@@ -141,27 +125,9 @@ struct CourseView: View {
         .rotation3DEffect(
             .degrees(Double(self.activeView.width / 10)),
             axis: (x: 0.0, y: 1.0, z: 0.0))
-        .hueRotation(.degrees(Double(self.activeView.width / 5)))
+//        .hueRotation(.degrees(Double(self.activeView.width / 5)))
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-        .gesture(
-            course.show ?
-                DragGesture()
-                .onChanged {value in
-                    guard value.translation.width < 300  else { return }
-                    guard value.translation.width > 0 else { return }
-                    self.activeView = value.translation
-                    
-                }
-                .onEnded {value in
-                    if self.activeView.width > 50 {
-                        self.course.show = false
-                        self.active = false
-                        self.activeIndex = -1
-                    }
-                    self.activeView = .zero
-                }
-                : nil
-        )
+        
         .edgesIgnoringSafeArea(.all)
     }
     
